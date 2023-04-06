83fe7dd24471d853124c90525bfd3dc25d77d1ed
Thomas Moraes <tho.vini7@gmail.com>
2023-04-06 20:42:37 -0300

    refatorando views e tratamento de erros com rotas

--- source/App/Web.php
+++ source/App/Web.php
@@ -2,6 +2,7 @@
 namespace Source\App;
 
 use Source\Core\Controller;
+use stdClass;
 
 class Web extends Controller
 {
@@ -12,15 +13,39 @@ class Web extends Controller
 
     public function home(): void
     {
+        $head = $this->seo->render(
+            CONF_SITE_NAME . " - " . CONF_SITE_TITLE,
+            CONF_SITE_DESC,
+            url(),
+            url("/assets/images/share,jpg"),
+        );
+
         echo $this->view->render("home", [
-            "title" => "CaféControl - Gerencie suas contas com o melhor café"
+            "head" => $head,
+            "video" => "sNBLOxxDPrc"
         ]);
     }
 
     public function error(array $data): void
     {
+        $error = new stdClass;
+        $error->code = $data['errcode'];
+        $error->title = "Ooops. Conteúdo indisponível :/";
+        $error->message = "Sentimes muito, mas o conteúdo que você tentou acessar não existe, está indposnível no momento ou foi removido :/";
+        $error->linkTitle = "Continue navegando!";
+        $error->link = url_back();
+
+        $head = $this->seo->render(
+            "{$error->code} | {$error->title}",
+            $error->message,
+            url("/ops/{$error->code}"),
+            url("/assets/images/share,jpg"),
+            false
+        );
+
         echo $this->view->render("error", [
-            "title" => "{$data['errcode']} | Ooops"
+            "head" => $head,
+            "error" => $error
         ]);
     }
 }
