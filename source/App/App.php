4daa8676f0afc85ff850a7bd008278660a654bd2
Thomas Moraes <tho.vini7@gmail.com>
2023-04-22 12:42:38 -0300

    Lancamentos

--- source/App/App.php
+++ source/App/App.php
@@ -38,6 +38,8 @@ class App extends Controller
 
         (new Access())->report();
         (new Online())->report();
+
+        (new AppInvoice)->fixed($this->user, 3);
     }
 
     /**
--- source/Models/CafeApp/AppInvoice.php
+++ source/Models/CafeApp/AppInvoice.php
@@ -1,14 +1,67 @@
 <?php 
 namespace Source\Models\CafeApp;
 
+use DateInterval;
+use DatePeriod;
+use DateTime;
 use Source\Core\Model;
 use Source\Models\User;
+use Source\Models\CafeApp\AppCategory;
 
 class AppInvoice extends Model
 {
     public function __construct()
     {
         parent::__construct("app_invoices", ['id'], ['user_id', 'wallet_id', 'category_id', 'description', 'type', 'value', 'due_at', 'repeat_when']);   
+    }
+
+    /**
+     * @param User $user
+     * @param integer $afterMonths
+     * @return void
+     */
+    public function fixed(User $user, int $afterMonths = 1): void
+    {
+       $fixed = $this->find("user_id = :user AND status = 'paid' AND type IN('fixed_income', 'fixed_expense')", "user={$user->id}")->fetch(true);
+
+        if(!$fixed) {
+            return;
+        }
+
+        foreach($fixed as $fixedItem) {
+            $invoice = $fixedItem->id;
+            $start = new DateTime($fixedItem->dueAt);
+            $end = new DateTime("+{$afterMonths}month");
+
+            if($fixedItem->period == "month") {
+                $interval = new DateInterval("P1M");
+            }
+
+            if($fixedItem->period == "year") {
+                $interval = new DateInterval("P1Y");
+            }
+            
+            $period = new DatePeriod($start, $interval, $end);
+            foreach($period as $item) {
+                $getFixed = $this->find("user_id = :user AND invoice_of = :of AND year(due_at) = :y AND month(due_at) = :m", 
+                "user={$user->id}&of={$fixedItem->id}&y={$item->format("Y")}&m={$item->format("m")}", "id")->fetch();
+
+                if(!$getFixed) {
+                    $newItem = $fixedItem;
+                    $newItem->id = null;
+                    $newItem->invoice_of = $invoice;
+                    $newItem->type = str_replace("fixed_", "", $newItem->type);
+                    $newItem->due_at = $item->format("Y-m-d");
+                    $newItem->status = ($item->format("Y-m-d") <= date("Y-m-d") ? "paid" : "unpaid");
+                    $newItem->save();
+                }
+            }
+
+
+            // var_dump($invoice, $start, $end);
+        }
+
+    //    var_dump($fixed);
     }
 
     /**
