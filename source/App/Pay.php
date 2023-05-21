9f2b2e34c2bc908f44631e4f71bd56b234743c59
Thomas Moraes <tho.vini7@gmail.com>
2023-05-21 18:27:49 -0300

    Criando assinatura

--- source/App/Pay.php
+++ source/App/Pay.php
@@ -1,7 +1,12 @@
 <?php 
 namespace Source\App;
 
+use Source\Models\Auth;
 use Source\Core\Controller;
+use Source\Models\CafeApp\AppCreditCard;
+use Source\Models\CafeApp\AppOrder;
+use Source\Models\CafeApp\AppPlan;
+use Source\Models\CafeApp\AppSubscription;
 
 class Pay extends Controller
 {
@@ -12,7 +17,53 @@ class Pay extends Controller
     
     public function create(array $data): void
     {
-        
+        $user = Auth::user();
+        $plan = (new AppPlan)->findById($data["plan"]);
+
+        if(request_limit('pay_create', 3, 60*5)) {
+            $json['message'] = $this->message->warning("Desculpe, {$user->first_name}, mas por segurança aguarde pelo menos 5 minutos para tentar outro cartão")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $checkSubscribe = (new AppSubscription)->findById("user_id = :user AND status != :status", "user={$user->id}&status=canceled")->fetch();
+
+        if($checkSubscribe) {
+            $json['message'] = $this->message->warning("Você já tem uma assinatura ativa {$user->first_name}. Não é necessário assinar o {$plan->name} mais uma vez.")->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $creditCard = new AppCreditCard;
+        $card = $creditCard->creditCard(
+            $user,
+            $data["card_number"],
+            $data["card_holder_name"],
+            $data["card_expiration_date"],
+            $data["card_cvv"]
+        );
+
+        if(!$card) {
+            $json['message'] = $creditCard->message()->before('Ooops! ')->after('. Favor verifique os dados para tentar assinar novamente.')->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $transaction = $card->transaction($plan->price);
+
+        if(!$transaction) {
+            $json['message'] = $card->message()->before('Ooops! ')->after('. Você pode tentar novamente com um novo cartão.')->render();
+            echo json_encode($json);
+            return;
+        }
+
+        $subscription = (new AppSubscription)->subscribe($user, $plan, $card);
+        (new AppOrder)->byCreditCard($user, $card, $subscription, $transaction);
+
+        $this->message->success("Bem-vindo(a) ao {$plan->name} {$user->first_name}. Sua assinatura está ativa e você já pode controlar. Confira os detalhe...")->flash();
+        $json['redirect'] = url('/app/assinatura');
+        echo json_encode($json);
+        return;
     }
 
     public function update(array $data): void
