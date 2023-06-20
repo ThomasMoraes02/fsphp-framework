a5cd8cec9d81459d3d143e86da30c5dcf9cb0df2
Thomas Moraes <tho.vini7@gmail.com>
2023-06-20 19:51:39 -0300

    Criando Post

--- source/App/Admin/Blog.php
+++ source/App/Admin/Blog.php
@@ -1,8 +1,11 @@
 <?php 
 namespace Source\App\Admin;
 
+use Source\Models\Category;
 use Source\Models\Post;
+use Source\Models\User;
 use Source\Support\Pager;
+use Source\Support\Upload;
 
 class Blog extends Admin
 {
@@ -29,6 +32,10 @@ class Blog extends Admin
         if(!empty($data['search']) && $data['search'] != "all") {
             $search = filter_var($data['search'], FILTER_SANITIZE_STRING);
             $posts = (new Post)->find("MATCH(title,subtitle) AGAINST(:s)", "s={$search}");
+            if(!$posts->count()) {
+                $this->message->info("Sua pesquisa não retornou resultados")->flash();
+                redirect(url("/admin/blog/home"));
+            }
         }
 
         $all = ($search ?? "all");
@@ -54,7 +61,44 @@ class Blog extends Admin
 
     public function post(?array $data): void
     {
-        
+        // MCE Upload
+        if(!empty($data['upload']) && !empty($_FILES['image'])) {
+            $files = $_FILES['image'];
+            $upload = new Upload();
+            $image = $upload->image($files, "post-".time());
+
+            if(!$image) {
+                $json['message'] = $upload->message()->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $json['mce_image'] = '<img style="width: 100%;" src="'.url("/storage/{$image}").'" title="{title}" alt="{title}">';
+            echo json_encode($json);
+            return;
+        }
+
+        $postEdit = null;
+        if(!empty($data['post_id'])) {
+            $postId = filter_var($data['post_id'], FILTER_SANITIZE_NUMBER_INT);
+            $postEdit = (new Post)->findById($postId);
+        }
+
+        $head = $this->seo->render(
+            CONF_SITE_NAME . " | " . ($postEdit->title ?? "Novo Artigo"),
+            CONF_SITE_DESC,
+            url("/admin"),
+            theme("/assets/images/image.jpg", CONF_VIEW_ADMIN),
+            false
+        );
+
+        echo $this->view->render("widgets/blog/post", [
+            "app" => "blog/post",
+            "head" => $head,
+            "post" => $postEdit,
+            "categories" => (new Category)->find("type = :type", "type=post")->order("title")->fetch(true),
+            "authors" => (new User)->find("level >= :level", "level=5")->fetch(true),
+        ]);
     }
 
     public function categories(?array $data): void
