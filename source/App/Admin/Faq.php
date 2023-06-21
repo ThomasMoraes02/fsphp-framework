e395f8ae0c513c3cf50edd311a7b2af7f8e7a4d0
Thomas Moraes <tho.vini7@gmail.com>
2023-06-21 20:21:44 -0300

    Crud Channel

--- source/App/Admin/Faq.php
+++ source/App/Admin/Faq.php
@@ -35,8 +35,74 @@ class Faq extends Admin
 
     public function channel(?array $data): void
     {
+        // Create
+        if(!empty($data['action']) && $data['action'] == "create") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+
+            $channelCreate = new Channel;
+            $channelCreate->channel = $data['channel'];
+            $channelCreate->description = $data['description'];
+
+            if($channelCreate->save()) {
+                $json['message'] = $channelCreate->message()->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $this->message->success("Canal cadastrado com sucesso...")->flash();
+            echo json_encode([
+                "redirect" => url("/admin/faq/channel/{$channelCreate->id}"),
+            ]);
+            return;
+        }
+
+        // Update
+        if(!empty($data['action']) && $data['action'] == "update") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+            $channelId = filter_var($data['channel_id'], FILTER_VALIDATE_INT);
+
+            $channelUpdate = (new Channel)->findById($channelId);
+            $channelUpdate->channel = $data['channel'];
+            $channelUpdate->description = $data['description'];
+
+            if($channelUpdate->save()) {
+                $json['message'] = $channelUpdate->message()->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $this->message->success("Canal atualizado com sucesso...")->flash();
+            echo json_encode([
+                "reload" => true
+            ]);
+
+            return;
+        }
+
+        // Delete
+        if(!empty($data['action']) && $data['action'] == "delete") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+            $channelId = filter_var($data['channel_id'], FILTER_VALIDATE_INT);
+
+            $channelDelete = (new Channel)->findById($channelId);
+            $channelDelete->destroy();
+
+            $this->message->success("Canal excluído com sucesso...")->flash();
+            echo json_encode([
+                "redirect" => url("/admin/faq/home")
+            ]);
+
+            return;
+        }
+
+        $channelEdit = null;
+        if(!empty($data['channel_id'])) {
+            $channelId = filter_var($data['channel_id'], FILTER_VALIDATE_INT);
+            $channelEdit = (new Channel)->findById($channelId);
+        }
+
         $head = $this->seo->render(
-            CONF_SITE_NAME . " | Faq: Novo Canal",
+            CONF_SITE_NAME . " | " . ($channelEdit ? "FAQ: {$channelEdit->channel}" : "FAQ: Novo Canal"),
             CONF_SITE_DESC,
             url("/admin"),
             theme("/assets/images/image.jpg", CONF_VIEW_ADMIN),
@@ -46,7 +112,7 @@ class Faq extends Admin
         echo $this->view->render("widgets/faqs/channel", [
             "app" => "faq/home",
             "head" => $head,
-            "channel" => ""
+            "channel" => $channelEdit
         ]);
     }
 
