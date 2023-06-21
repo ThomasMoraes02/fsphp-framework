298172da2a187e5c9d31c8497912e3296d551621
Thomas Moraes <tho.vini7@gmail.com>
2023-06-21 20:31:33 -0300

    Crud Questions

--- source/App/Admin/Faq.php
+++ source/App/Admin/Faq.php
@@ -3,6 +3,7 @@ namespace Source\App\Admin;
 
 use Source\Support\Pager;
 use Source\Models\Faq\Channel;
+use Source\Models\Faq\Question;
 
 class Faq extends Admin
 {
@@ -118,8 +119,84 @@ class Faq extends Admin
 
     public function question(?array $data): void
     {
+        // Create
+        if(!empty($data['action']) && $data['action'] == "create") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+
+            $questionCreate = new Question;
+            $questionCreate->channel_id = $data['channel_id'];
+            $questionCreate->question = $data['question'];
+            $questionCreate->response = $data['response'];
+            $questionCreate->order_by = $data['order_by'];
+
+            if($questionCreate->save()) {
+                $json['message'] = $questionCreate->message()->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $this->message->success("Pergunta cadastrada com sucesso...")->flash();
+            echo json_encode([
+                "redirect" => url("/admin/faq/channel/{$questionCreate->channel_id}/{$questionCreate->question_id}"),
+            ]);
+            return;
+        }
+        
+        // Update
+        if(!empty($data['action']) && $data['action'] == "update") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+            $questionId = filter_var($data['question_id'], FILTER_VALIDATE_INT);
+
+            $questionEdit = (new Question)->findById($questionId);
+            $questionEdit->channel_id = $data['channel_id'];
+            $questionEdit->question = $data['question'];
+            $questionEdit->response = $data['response'];
+            $questionEdit->order_by = $data['order_by'];
+
+            if($questionEdit->save()) {
+                $json['message'] = $questionEdit->message()->render();
+                echo json_encode($json);
+                return;
+            }
+
+            $this->message->success("Pergunta atualizada com sucesso...")->flash();
+            echo json_encode([
+                "redirect" => url("/admin/faq/channel/{$questionEdit->channel_id}/{$questionEdit->question_id}"),
+            ]);
+
+            return;
+        }
+        
+        // Delete
+        if(!empty($data['action']) && $data['action'] == "delete") {
+            $data = filter_var_array($data, FILTER_SANITIZE_STRING);
+            $questionId = filter_var($data['question_id'], FILTER_VALIDATE_INT);
+
+            $questionDelete = (new Question)->findById($questionId);
+            $questionDelete->destroy();
+
+            $this->message->success("Canal excluído com sucesso...")->flash();
+            echo json_encode([
+                "redirect" => url("/admin/faq/home")
+            ]);
+            return;
+        }
+
+        $channel = (new Channel)->findById(filter_var($data['channel_id'], FILTER_VALIDATE_INT));
+        $question = null;
+
+        if(!$channel) {
+            $this->message->warning("Você tentou gerenciar perguntas um canal que não existe")->flash();
+            echo json_encode(["redirect" => url("/admin/faq/home")]);
+            return;
+        }
+
+        if(!empty($data['question_id'])) {
+            $question = (new Question)->findById(filter_var($data['question_id'], FILTER_VALIDATE_INT));
+        }
+
         $head = $this->seo->render(
-            CONF_SITE_NAME . " | Faq: Nova Pergunta",
+            CONF_SITE_NAME . " | Faq: Perguntad em {$channel->channel}",
             CONF_SITE_DESC,
             url("/admin"),
             theme("/assets/images/image.jpg", CONF_VIEW_ADMIN),
@@ -129,10 +206,8 @@ class Faq extends Admin
         echo $this->view->render("widgets/faqs/question", [
             "app" => "faq/home",
             "head" => $head,
-            "channel" => (object) [
-                "id" => 10
-            ],
-            "question" => ""
+            "channel" => $channel,
+            "question" => $question
         ]);
     }
 }
