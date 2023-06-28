52084df8c4b1d47f20cef9a7af6d79eea1511bcd
Thomas Moraes <tho.vini7@gmail.com>
2023-06-28 20:42:12 -0300

    Controller Api

--- source/App/CafeApi/CafeApi.php
+++ source/App/CafeApi/CafeApi.php
@@ -1,19 +1,27 @@
 <?php 
 namespace Source\App\CafeApi;
 
-use Source\Core\Controller;
 use Source\Models\Auth;
-use Source\Models\CafeApp\AppInvoice;
+use Source\Models\User;
+use Source\Core\Controller;
 use Source\Models\CafeApp\AppWallet;
+use Source\Models\CafeApp\AppInvoice;
 
 class CafeApi extends Controller
 {
+    /** @var User|null */
     protected $user;
 
+    /** @var array|false */
     protected $headers;
 
+    /** @var array|null */
     protected $response;
 
+    /**
+     * CafeApi constructor
+     * @throws Exception
+     */
     public function __construct()
     {
         parent::__construct("/");
@@ -21,13 +29,18 @@ class CafeApi extends Controller
         header('Content-Type: application/json; charset=UTF-8');
         $this->headers = getallheaders();
 
+        $request = $this->requestLimit("cafeApi", 1,1);
+        if(!$request) {
+            exit;
+        }
+
         $auth = $this->auth();
         if(!$auth) {
             exit;
         }
 
         (new AppWallet)->start($this->user);
-        (new AppInvoice)->findById($this->user, 3);
+        (new AppInvoice)->fixed($this->user, 3);
     }
 
     /**
@@ -70,8 +83,15 @@ class CafeApi extends Controller
     /**
      * @return boolean
      */
-    protected function auth(): bool
+    private function auth(): bool
     {
+        $endpoint = ["cafeApiAuth", 3, 60];
+        $request = $this->requestLimit($endpoint[0], $endpoint[1], $endpoint[2], true);
+
+        if(!$request) {
+            return false;
+        }
+
         if(empty($this->headers['email']) || empty($this->headers['password'])) {
             $this->call(400, "auth_empty", "Favor informe seu e-mail e senha")->back();
             return false;
@@ -81,11 +101,79 @@ class CafeApi extends Controller
         $user = $auth->attempt($this->headers['email'], $this->headers['password'], 1);
 
         if(!$user) {
+            $this->requestLimit($endpoint[0], $endpoint[1], $endpoint[2]);
+
             $this->call(401, "invalid_auth", $auth->message->getText())->back();
             return false;
         }
 
         $this->user = $user;
+        return true;
+    }
+
+    /**
+     * @param string $endpoint
+     * @param integer $limit
+     * @param integer $seconds
+     * @param boolean $attempt
+     * @return boolean
+     */
+    protected function requestLimit(string $endpoint, int $limit, int $seconds, bool $attempt = false): bool
+    {
+        $userToken = (!empty($this->headers['email']) ? base64_encode($this->headers['email']) : null);
+
+        if(!$userToken) {
+            $this->call(400, "invalid_data", "Favor informe seu e-mail e senha")->back();
+            return false;
+        }
+
+        $cacheDir = __DIR__ . "/../../../".CONF_UPLOAD_DIR."/requests";
+        if(!file_exists($cacheDir) || !is_dir($cacheDir)) {
+            mkdir($cacheDir, 0755, true);
+        }
+
+        $cacheFile = "{$cacheDir}/{$userToken}.json";
+        if(!file_exists($cacheFile) || !is_file($cacheFile)) {
+            fopen($cacheFile, "w");
+        }
+
+        $userCache = json_decode(file_get_contents($cacheFile));
+        $cache = (array) $userCache;
+
+        $save = function($cacheFile, $cache) {
+            $saveCache = fopen($cacheFile, "w");
+            fwrite($saveCache, json_encode($cache, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+            fclose($saveCache);
+        };
+
+        if(empty($cache[$endpoint]) || $cache[$endpoint]->time <= time()) {
+            if(!$attempt) {
+                $cache[$endpoint] = [
+                    "limit" => $limit,
+                    "request" => 1,
+                    "time" => time() + $seconds
+                ];
+
+                $save($cacheFile, $cache);
+            }
+
+            return true;
+        }
+
+        if($cache[$endpoint]->requests >= $limit) {
+            $this->call(400, "request_limit", "Limite de requisições atingido")->back();
+            return false;
+        }
+
+        if(!$attempt) {
+            $cache[$endpoint] = [
+                "limit" => $limit,
+                "requests" => $cache[$endpoint]->requests + 1,
+                "time" => $cache[$endpoint]->time
+            ];
+
+            $save($cacheFile, $cache);
+        }
         return true;
     }
 }
